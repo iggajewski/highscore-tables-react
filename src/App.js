11396968a225e2083bd24b8e7168f735e155d651
iggajewski <64140239+iggajewski@users.noreply.github.com>
2020-10-02 14:31:41 +0100

    lvl 5 finished

--- src/App.js
+++ src/App.js
@@ -1,30 +1,51 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import allCountryScores from './scores';
+
+let worldwideScores = [];
+for(let i = 0; i < allCountryScores.length; ++i) {
+  for(let j = 0; j < allCountryScores[i].scores.length; ++j) {
+    worldwideScores.push(allCountryScores[i].scores[j]);
+  }
+}
+
+function sortScores(asc) {
+  allCountryScores.sort((a, b) => a.name > b.name);
+  allCountryScores.map(country => country.scores.sort((a, b) => (asc ? a.s - b.s : b.s - a.s)));
+  worldwideScores.sort((a, b) => (asc ? a.s - b.s : b.s - a.s));
+}
 
 function CountryScores(props) {
   return (
   <div class="country">
     <h2>HIGH SCORES: {props.country.name}</h2>
     <div class="country-table">
-
       {props.country.scores.map(score => (
         <div class="individual-score">
           <p class="name-data">{score.n}</p>
           <p class="score-data">{score.s}</p>
         </div>
       ))}
-      
     </div>
   </div>
   );
 }
 
 function App() {
+  let [sortOrder, setSortOrder] = useState(false);
+  function changeOrder() {
+    setSortOrder(!sortOrder);
+    sortScores(sortOrder);
+  }
+
+  sortScores(sortOrder);
+
   return (
     <div id="page">
       <h1>High Scores per Country</h1>
+      <button onClick={changeOrder}>Change Score Order</button>
       <div id="contents">
+        <CountryScores country={{name: "Worldwide", scores:worldwideScores}}/>
         {allCountryScores.map(country => (<CountryScores country={country}/>))}
       </div>
     </div>
@@ -32,29 +53,3 @@ function App() {
 }
 
 export default App;
-
-/*
-TODO:
-
-Level 2
-
-    Present the High Score Tables sorted alphabetically by country name
-
-Level 3
-
-    Within each individual table, show the highest scores first - i.e. sort the scores numerically, descending.
-
-Level 4 - advanced (needs content from React week 2)
-
-    Add a single button at the top of the page which toggles the sort order of every high-score-table between ascending and descending by scores. This will require some study about event handlers and the use of a "State hook".
-
-Level 5 - advanced
-
-    Add a "world-wide" table, shown first, which shows the sorted high scores from EVERYONE, regardless of country.
-
-Bonus
-
-    Add your own country and scores!
-    Add some old-skool videogame icons or animations
-
-*/
